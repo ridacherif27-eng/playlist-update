8edfb0317ca26e8fbed05d5280c24b1456debdbc
ridacherif27-eng <ridacherif27@gmail.com>
2026-06-30 17:29:02 +0100

    Update make_adult.py

--- make_adult.py
+++ make_adult.py
@@ -24,3 +24,18 @@ CHANNELS_DATA = [
     {"name": "XXL TV", "url": "http://adulttv.host:8080/live/adult/adult/18.m3u8"},
     {"name": "CentoXCento TV", "url": "http://adulttv.host:8080/live/adult/adult/19.m3u8"},
     {"name": "Playboy TV", "url": "http://adulttv.host:
+import os
+import requests
+import re
+
+OUTPUT_FILE = "adult_playlist.m3u"
+
+# باقة القنوات الشاملة والثابتة المأخوذة من سيرفرات بث مستقرة ومفتوحة بدون حظر
+CHANNELS_DATA = [
+    {"name": "Brazzers TV HD", "url": "http://adulttv.host:8080/live/adult/adult/1.m3u8"},
+    {"name": "Brazzers TV Europe", "url": "http://adulttv.host:8080/live/adult/adult/2.m3u8"},
+    {"name": "VIXEN", "url": "http://adulttv.host:8080/live/adult/adult/3.m3u8"},
+    {"name": "FAKE TAXI", "url": "http://adulttv.host:8080/live/adult/adult/4.m3u8"},
+    {"name": "Private TV HD", "url": "http://adulttv.host:8080/live/adult/adult/5.m3u8"},
+    {"name": "Hustler TV HD", "url": "http://adulttv.host:8080/live/adult/adult/6.m3u8"},
+    {"name": "Blue Hustler", "url": "
